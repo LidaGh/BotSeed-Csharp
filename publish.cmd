3fc8cc8ada27585d117fc6bf24c16a0b4aa921eb
LidaGh <lida.ghahremanloo@gmail.com>
2018-11-22 11:41:59 +0000

    Add all files

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=luis-c-botseed-lida-Web-Deploy.pubxml -p:Password=3RSmkiuemrwATyEC7RnCekia8qMyZ7FdThFrt2YBjwA1pkuNZkXcdo7qfx46
+
